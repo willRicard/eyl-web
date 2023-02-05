b51ed8d3b728b0c2fd9b32747f8a90422f659dde
grouab <abderrahmane.grou@polymtl.ca>
2023-02-05 10:40:35 -0500

    Project Web UI

--- eyl-web/src/app/classes/seuils.ts
+++ eyl-web/src/app/classes/seuils.ts
@@ -1,12 +1,13 @@
-export const seuils = {
+export const rawDailyNeeds = {
     'Dietary Fiber': '10.0 g', 
     'Sugars': '100.0 g',
+    'Protein': '100.0 g',
     'Soluble Fiber': '40.0 g', 
     'Monounsaturated Fat': '5 g', 
     'Polyunsaturated Fat': '9 g', 
     'Trans Fat': '10.0 g', 
-    'Other Carbohydrate': '120.0 g',
+    'Other Carbohydrate': '12 g',
     'Sodium': '0.1 g'
   };
 
-export const seuilsArray = Object.entries(seuils).map(([key, value]) => ({ key, value }));
+export const arrayDailyNeeds = Object.entries(rawDailyNeeds).map(([key, value]) => ({ key, value }));
--- eyl-web/src/app/pages/home/home.component.ts
+++ eyl-web/src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { seuilsArray } from 'src/app/classes/seuils';
+import { arrayDailyNeeds } from 'src/app/classes/seuils';
 
 @Component({
   selector: 'app-home',
@@ -8,27 +8,40 @@ import { seuilsArray } from 'src/app/classes/seuils';
 })
 export class HomeComponent {
   nutritionValues = {
-    'Dietary Fiber': '2.0 g', 
-    'Sugars': '9.0 g',
+    'Dietary Fiber': '0.0 g', 
+    'Sugars': '25.0 g',
+    'Protein': '1.0 g',
     'Soluble Fiber': '1.0 g', 
-    'Monounsaturated Fat': '0.5 g', 
-    'Polyunsaturated Fat': '0.5 g', 
-    'Trans Fat': '0.0 g', 
-    'Other Carbohydrate': '11.0 g'
+    'Monounsaturated Fat': '0.0 g', 
+    'Polyunsaturated Fat': '0.0 g', 
+    'Trans Fat': '0.0 g' 
   };
 
   nutritionArray = Object.entries(this.nutritionValues).map(([key, value]) => ({ key, value }));
-  nutritionUnits: number[] = [];
+  nutritionUnits: { [id: string]: number; }[] = [];
+  dailyNeeds: { [id: string]: number; }[] = [];
   progressUnits: number[] = [];
   progress = 30;
 
   constructor() {
+    let obj: { [id: string]: number; } = {};
+
     this.nutritionArray.map((item) => {
-      this.nutritionUnits.push(parseFloat(item.value))
+      obj = {};
+      obj[item.key] = parseFloat(item.value);
+      this.nutritionUnits.push(obj);
     })
 
-    seuilsArray.map((item) => {
-      this.progressUnits.push(this.nutritionUnits[seuilsArray.indexOf(item)] / parseFloat(item.value) * 100)
+    arrayDailyNeeds.map((item) => {
+      obj = {};
+      obj[item.key] = parseFloat(item.value);
+      this.dailyNeeds.push(obj);
+    })
+
+    this.nutritionUnits.map((item) => {
+      let nutriFact = this.dailyNeeds.find((nutriFact) => Object.keys(nutriFact)[0] == Object.keys(item)[0]);
+      if (nutriFact)
+        this.progressUnits.push(Object.values(item)[0] / Object.values(nutriFact)[0] * 100)
     })
   }
 
